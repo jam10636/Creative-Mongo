f929080daf99c09fec245c18251e9588a6dbd2ed
kevin tsou <jam10636@gmail.com>
2017-11-12 05:26:38 +0000

    Update

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -15,6 +15,7 @@ $("#done").html(textStatus)}
       var myobj = {Name:$("#name").val(),Phone:$("#phone").val(),Email:$("#email").val(),Photo:$('#photo').val(),Address:$("#address").val()};
       jobj = JSON.stringify(myobj);
         $("#json").text(jobj);
+	$('#comments').html('Success!');
 var url = "address";
 $.ajax({
 url:url,
@@ -23,38 +24,40 @@ data: jobj,
 contentType: "application/json; charset=utf-8",
 success: function(data,textStatus) {
 }
-})
-$.getJSON('address', function(data) {
-      console.log(data);
-      var everything = "<ul>";
-      for(var comment in data) {
-        imgurl="";
-	com = data[comment];
-	if(com.Photo=="")
-	{
+});
+$("#getComments").click(function() {
+     $.getJSON('address', function(data) {
+        console.log(data);
+        var everything = "<ul>";
+        for(var comment in data) {
+          com = data[comment];
+	  imgurl="";
+ 	if(com.Photo=="")
+        {
        imgurl="http://funny-pics.co/wp-content/uploads/Funny-Alpaca-Face.jpg"
-	}	
-	else
-	{	imgurl=com.Photo};
-        everything+='<div class="contact">' +
-          '<div>' +
-            '<span class="contactTitle">' +
-              '<img src="'+imgurl+'"/>' +
-            '</span>' +
-            '<span class="contactTitle">' +
-              '<h4>'+
-                com.Name+'</h4>' +
-            '</span>' +
-                '</div>' +
-          com.Phone +'<br>'+
-          '<a href="mailto:'+com.Email+'">'+com.Email+'</a><br>' +
-          com.Address+'<br>' +
-          '<input type="checkbox" ng-model="contact.done">'+
-        '</div>'
-}
-      everything += "</ul>";
-      $("#comments").html(everything);
-    })
-
+        }
+        else
+        {       imgurl=com.Photo};
+	everything+='<div class="contact">' +
+            '<div>' +
+              '<span class="contactTitle">' +
+                '<img src="'+imgurl+'"/>' +
+              '</span>' +
+              '<span class="contactTitle">' +
+                '<h4>'+
+ 		com.Name+'</h4>' +
+              '</span>' +
+                 '</div>' +
+            com.Phone +'<br>'+
+            '<a href="mailto:'+com.Email+'">'+com.Email+'</a><br>' +
+            com.Address+'<br>' +
+            '<input type="checkbox" ng-model="contact.done">'+
+        '</div>'      
+         '</div>'
+  }
+        everything += "</ul>";
+        $("#comments").html(everything);
+      })
+   })
 });
 });
