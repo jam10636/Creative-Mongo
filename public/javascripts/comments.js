ffce0fa431c64e796e424fe05e0d609e5a7e5114
kevin tsou <jam10636@gmail.com>
2017-11-12 05:36:39 +0000

    update

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -15,7 +15,6 @@ $("#done").html(textStatus)}
       var myobj = {Name:$("#name").val(),Phone:$("#phone").val(),Email:$("#email").val(),Photo:$('#photo').val(),Address:$("#address").val()};
       jobj = JSON.stringify(myobj);
         $("#json").text(jobj);
-	$('#comments').html('Success!');
 var url = "address";
 $.ajax({
 url:url,
@@ -25,6 +24,39 @@ contentType: "application/json; charset=utf-8",
 success: function(data,textStatus) {
 }
 });
+ $.getJSON('address', function(data) {
+        console.log(data);
+        var everything = "<ul>";
+        for(var comment in data) {
+          com = data[comment];
+          imgurl="";
+        if(com.Photo=="")
+        {
+       imgurl="http://funny-pics.co/wp-content/uploads/Funny-Alpaca-Face.jpg"
+        }
+        else
+        {       imgurl=com.Photo};
+        everything+='<div class="contact">' +
+            '<div>' +
+              '<span class="contactTitle">' +
+                '<img src="'+imgurl+'"/>' +
+              '</span>' +
+              '<span class="contactTitle">' +
+                '<h4>'+
+                com.Name+'</h4>' +
+              '</span>' +
+                 '</div>' +
+            com.Phone +'<br>'+
+            '<a href="mailto:'+com.Email+'">'+com.Email+'</a><br>' +
+            com.Address+'<br>' +
+            '<input type="checkbox" ng-model="contact.done">'+
+        '</div>'
+         '</div>'
+  }
+        everything += "</ul>";
+        $("#comments").html(everything);
+      })
+
 });
 $("#getComments").click(function() {
      $.getJSON('address', function(data) {
