1a7e70b81307408126702b4636e71c11213c7c7e
kevin tsou <jam10636@gmail.com>
2017-11-14 05:57:07 +0000

    little update

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
- $('#deleteComments').click(function(){
+$('#deleteComments').click(function(){
+var checkbox1=['comment'];
 var url="address";
+jobj = JSON.stringify(checkbox1);
 $.ajax({
 url:url,
+data:jobj,
 type:"DELETE",
+contentType: "application/json; charset=utf-8",
 success: function(data,textStatus){
 everything="";
-      $("#comments").html(everything);
+$("#comments").html(everything);
 $("#json").text("");
 $("#done").html(textStatus)}
 })
 });
  $("#postComment").click(function(){
-      var myobj = {Name:$("#name").val(),Phone:$("#phone").val(),Email:$("#email").val(),Photo:$('#photo').val(),Address:$("#address").val()};
+var imgurl;	
+if($('#photo').val()=="")
+	{
+imgurl="http://funny-pics.co/wp-content/uploads/Funny-Alpaca-Face.jpg"
+}
+else
+{
+imgurl=$('#photo').val()
+}
+      var myobj = {Check:false,Name:$("#name").val(),Phone:$("#phone").val(),Email:$("#email").val(),Photo:imgurl,Address:$("#address").val()};
       jobj = JSON.stringify(myobj);
         $("#json").text(jobj);
 var url = "address";
@@ -29,17 +42,10 @@ success: function(data,textStatus) {
         var everything = "<ul>";
         for(var comment in data) {
           com = data[comment];
-          imgurl="";
-        if(com.Photo=="")
-        {
-       imgurl="http://funny-pics.co/wp-content/uploads/Funny-Alpaca-Face.jpg"
-        }
-        else
-        {       imgurl=com.Photo};
         everything+='<div class="contact">' +
             '<div>' +
               '<span class="contactTitle">' +
-                '<img src="'+imgurl+'"/>' +
+                '<img src="'+com.Photo+'"/>' +
               '</span>' +
               '<span class="contactTitle">' +
                 '<h4>'+
@@ -49,32 +55,24 @@ success: function(data,textStatus) {
             com.Phone +'<br>'+
             '<a href="mailto:'+com.Email+'">'+com.Email+'</a><br>' +
             com.Address+'<br>' +
-            '<input type="checkbox" ng-model="contact.done">'+
-        '</div>'
-         '</div>'
-  }
+        '<input class="checkedbox" type="checkbox" id='+com._id+'>'+
+	'</div>'}
         everything += "</ul>";
         $("#comments").html(everything);
-      })
 
+});
 });
 $("#getComments").click(function() {
      $.getJSON('address', function(data) {
         console.log(data);
         var everything = "<ul>";
+	var count=0;
         for(var comment in data) {
           com = data[comment];
-	  imgurl="";
- 	if(com.Photo=="")
-        {
-       imgurl="http://funny-pics.co/wp-content/uploads/Funny-Alpaca-Face.jpg"
-        }
-        else
-        {       imgurl=com.Photo};
 	everything+='<div class="contact">' +
             '<div>' +
               '<span class="contactTitle">' +
-                '<img src="'+imgurl+'"/>' +
+                '<img src="'+com.Photo+'"/>' +
               '</span>' +
               '<span class="contactTitle">' +
                 '<h4>'+
@@ -84,12 +82,33 @@ $("#getComments").click(function() {
             com.Phone +'<br>'+
             '<a href="mailto:'+com.Email+'">'+com.Email+'</a><br>' +
             com.Address+'<br>' +
-            '<input type="checkbox" ng-model="contact.done">'+
-        '</div>'      
-         '</div>'
-  }
+            '<input class="checkedbox" type="checkbox" id='+com._id+'>'+
+         '</div>'};
         everything += "</ul>";
         $("#comments").html(everything);
-      })
    })
+});
+$('#delete').click(function(){
+var checkbox=[];
+$.getJSON('address', function(data) {
+ $(".checkedbox").each(function()
+        {
+            if($(this).is(':checked'))
+            {
+		checkbox.push(this.id);
+		}
+        })
+jobj = JSON.stringify(checkbox);
+var url = "address";
+$.ajax({
+url:url,
+type: "DELETE",
+data: jobj,
+contentType: "application/json; charset=utf-8",
+success: function(data,textStatus) {
+}
+
+});
+});
+});
 });
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ Name: String,
 Email: String,
 Phone: String,
 Address: String,
-Photo: String
+Photo: String,
+Check: Boolean
 });
 
 var Address = mongoose.model('Address', commentSchema); //Makes an object from that schema as a model
@@ -20,7 +21,7 @@ console.log('Connected');
 });
 /* GET home page. */
 router.post('/address', function(req, res, next) {
-var newaddress = new Address(req.body); //[3]
+var newaddress = new Address(req.body);
 newaddress.save(function(err, post) { //[4]
   if (err) return console.error(err);
   res.sendStatus(200);
@@ -31,14 +32,26 @@ Address.find(function(err,commentList) { //Calls the find() method on your datab
   if (err) return console.error(err); //If there's an error, print it out
   else {
    res.json(commentList);
-
   }
 });
 });
 router.delete('/address',function(req,res,next){
+if(req.body[0]=='comment')
+{
 Address.remove(function(err){
 if(err) return console.error(err)
-else res.sendStatus(200);
+})
+}
+else{
+for(i=0;i<req.body.length;i++)
+{
+Address.findByIdAndRemove(req.body[i], function (err,offer){
+    if(err) { throw err; }
+
 });
+}
+}    // ...
+res.sendStatus(200);
+
 });
 module.exports = router;
